9dc058b9557d3610ab87da99217c88019c663f51
unknown <evandriridho321@gmail.com>
2024-08-27 14:44:07 +0700

    Test Vitest

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import Error from "./components/Fragments/NotFound404";
+import Error from "./components/Fragments/ErrorPage";
 import "./index.css";
 import HomePage from "./pages/HomePage";
 import LoginPage from "./pages/LoginPage";
