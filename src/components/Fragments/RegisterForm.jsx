bddb7579ca13df9f2a9af2d1c2d26727701de496
unknown <evandriridho321@gmail.com>
2024-08-27 20:15:29 +0700

    Solve Problem

--- src/components/Fragments/RegisterForm.jsx
+++ src/components/Fragments/RegisterForm.jsx
@@ -1,5 +1,5 @@
 import { Link } from "react-router-dom";
-import Button from "../Elements/Button/Button";
+import Button from "../Elements/Button/button";
 import InputForm from "../Elements/Input/Index";
 
 const RegisterForm = () => {
